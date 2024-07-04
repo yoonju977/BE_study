a53a10fe5545a5997686707e078b3737deb478e4
yoonju977 <yoonju977@gmail.com>
2024-07-04 11:34:22 +0900

    전체 수정

--- polls/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("<int:question_id>/", views.detail, name="detail"),
-]
-# ex: /polls/5/ path("<int:question_id>/", views.detail, name="detail"),
--- polls/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def detail(request, question_id): 
-    return HttpResponse("You're looking at question %s." % question_id)
